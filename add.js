7978a5fc0a504a8da1225bef58286a3359f30080
Nitin Mandolkar <nitinmandolkar@gmail.com>
2019-05-02 06:49:28 +0000

    add-as-a-service

---
+++ add.js
@@ -0,0 +1,35 @@
+// ************************************************************
+// Add-as-a-service
+// ************************************************************
+//
+'use strict';
+
+function add(x, y) { 
+
+    // test cases
+    // x = 1 and y = 2 should return 3
+
+    // implement here
+    // ...
+    // return ...
+    
+    return x+y;
+    
+    //throw new Error('Not Implemented');
+}
+
+exports.handler = function(event, context, cb) {
+    
+    var x = event["x"], y = event["y"];
+    
+    var result = add(x,y);
+   
+    cb(null, { 
+        statusCode: 200, 
+        body: JSON.stringify({'result' : result }),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    })
+};
